1bd07fd2e2f655e7099deed1ad4fa769f854ac61
tabish-nadeem <tabish.nadeem@atmecs.com>
2022-09-14 13:23:19 +0530

    notifications-menu-po file cleaned

--- e2e/pageObjects/notification-menu.po.ts
+++ e2e/pageObjects/notification-menu.po.ts
@@ -16,15 +16,8 @@ export class NotificationMenuPO {
     };
   }
 
-  async openNotificationMenu() {
-    await this.elements.openCloseMenuButton.click();
-    await expect(this.page.locator('.notification-popover-wrapper')).waitFor({state:'attached',timeout:this.DEFAULT_WAIT});
 
-  }
 
-  async isNotificationMenuOpened() {
-    return await this.elements.notificationsMenuPopover.isDisplayed();
-  }
 
   async isBadgeDisplayed() {
     return await this.elements.badge.isDisplayed();
@@ -35,48 +28,9 @@ export class NotificationMenuPO {
   }
 
   async getAmountOfUnreadNotifications() {
-    let num = await this.elements.badge.getText();
+    let num = await this.elements.badge.textContent();
     return parseInt(num);
   }
 
-  getNotificationElementByIndex = async index => {
-    let notifications = await this.elements.notificationsList;
-    return notifications[index];
-  }
-
-  getNotificationInfoByIndex = async index => {
-    let notificationInfo = {};
-    let notification = await this.getNotificationElementByIndex(index);
-    if (notification) {
-      let results = await Promise.all([notification.locator('.notificationItemTitle').getText(),
-        notification.locator('.notificationItemDescription').getText(),
-        notification.locator('.notificationItemPublisher').getText(),
-        notification.locator('.notificationItemDate').getText()]);
-      notificationInfo = {
-        title: results[0],
-        description: results[1],
-        publisher: results[2],
-        date: results[3]
-      };
-    }
-    return notificationInfo;
-  }
-
-  waitForBadgeToBeDisplayed = async timeToWait => {
-    // const EC = protractor.ExpectedConditions;
-    // return browser.wait(EC.visibilityOf(this.elements.badge), timeToWait || 5000);
-    return await expect(this.elements.badge).toBeVisible(timeToWait || 5000);
-  }
-
-  clickOnLatestNotification = async () => {
-    const isOpened = await this.isNotificationMenuOpened();
-
-    if (!isOpened) {
-      await this.openNotificationMenu();
-    }
-
-    let firstMsg = await this.getNotificationElementByIndex(0);
-    return firstMsg.click();
-  };
 
 }
