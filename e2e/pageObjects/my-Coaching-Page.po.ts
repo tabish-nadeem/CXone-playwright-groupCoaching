97905599d06053aa9b90289cca216ef78b44b5db
tabish-nadeem <tabish.nadeem@atmecs.com>
2022-09-14 12:56:57 +0530

    my-coaching-page-po cleaned

--- e2e/pageObjects/my-Coaching-Page.po.ts
+++ e2e/pageObjects/my-Coaching-Page.po.ts
@@ -39,14 +39,6 @@ export class MyCoachingsPo {
         };
     }
 
-    public async getSearchPlaceholderText() {
-        await this.page.waitForSelector(this.elements.searchField);
-        return await this.elements.searchField.getAttribute('placeholder');
-    }
-
-    public async isSearchPresent() {
-        return await this.elements.searchField.isPresent();
-    }
 
     public async getRowElements(columnId: any,columnValue: any) {
         await CommonUIUtils.waitUntilIconLoaderDone(this.page);
@@ -78,39 +70,11 @@ export class MyCoachingsPo {
     }
 
     public async navigate(){
-        let baseUrl = this.uiConstants.URLS.LOCALHOST
+        // let baseUrl = this.uiConstants.URLS.LOCALHOST
         // await this.page.goto(baseUrl + URLs);
         // await this.page.waitForURL('**\/#/manageForms');
         // await CommonUIUtils.waitUntilIconLoaderDone(this.page);
         // await this.page.waitForSelector(`#ng2-manage-forms-page`);
-    }
-
-    // public async navigateTo(quickly?: boolean) {
-    //     if (quickly) {
-    //         return await navigateQuicklyTo(protractorConfig.fdUtils.getPageIdentifierUrls('coaching.myCoaching'), this.ancestor);
-    //     } else {
-    //         return await navigateTo(protractorConfig.fdUtils.getPageIdentifierUrls('coaching.myCoaching'), this.ancestor);
-    //     }
-    // }
-
-    public async waitForMyCoachingsPage () {
-        await expect(this.elements.coachingPlanContainer).toBeVisible(60000);
-        await this.utils.waitForSpinnerToDisappear();
-    }
-
-    public async isCoachingRowPresent(coachingPlan) {
-        console.log('plan name',coachingPlan);
-        return await this.elements.coachingSession(coachingPlan).isPresent();
-    }
-
-    public async verifyCoachingCompletedStatus(coachingPlanName, statusToVerify) {
-        let rowEle = await this.getCoachingRowElements(coachingPlanName);
-        return (rowEle.status === statusToVerify);
-    }
-
-    public async clickOnActionBtn(btnText) {
-        await protractorConfig.testUtils.waitForItemToBeClickable(this.elements.actionBtns(btnText), 15000);
-        return await this.elements.actionBtns(btnText).click();
     }
 
     public async clickSubmitButton() {
@@ -120,10 +84,6 @@ export class MyCoachingsPo {
         await this.utils.waitForSpinnerToDisappear();
     }
 
-    public async getPageTitleText() {
-        await this.page.waitForSelector(this.elements.pageTitle);
-        return await this.elements.pageTitle.getText();
-    }
 
     public async openCoachingSession(coachingPlan) {
         await this.page.waitForSelector(this.elements.coachingSession(coachingPlan),30000);
@@ -132,81 +92,5 @@ export class MyCoachingsPo {
         await this.utils.waitForSpinnerToDisappear();
     }
 
-    public async getCoachingRowElements (value) {
-        console.log('undefined --->', value);
-        let temp = await this.page.locator('xpath=.//*[text()="' + value + '"]/../../../..');
-        await this.page.waitForSelector(temp, 20000);
-        return {
-            assignedBy: await temp.locator('[col-id="assignedBy"]').getText(),
-            remainingDays: await temp.locator('[col-id="remainingDays"]').getText(),
-            status: await temp.locator('[col-id="status"]').getText()
-        };
-    }
 
-    public async clickOnViewIcon (value) {
-        let temp = await this.page.locator('xpath=.//*[text()="' + value + '"]/../../../..');
-        await this.page.waitForSelector(temp, 20000);
-        await temp.locator('.action-btn.action-goto').click();
-        await this.page.waitForSelector(fdUtils.getPageIdentifierUrls('coaching.coachingExecutor'));
-        await this.utils.waitForSpinnerToDisappear();
-    }
-
-    public async refresh() {
-        // await refresh(this.ancestor);
-        await this.utils.waitABit(5000);
-    }
-
-    public async getRowCount() {
-        const label = await this.elements.countLabel.getText();
-        return +label.split(' ')[0];
-    }
-
-    public async searchCoaching(query: string) {
-        await this.elements.search.clear();
-        await this.elements.search.sendKeys(query);
-        await this.utils.delay(1000);
-    }
-
-    public async clearSearch() {
-        await this.elements.clearSearch.click();
-        await this.utils.delay(1000);
-    }
-
-    public async getCoachingIdFromField(columnId: string, query: string): Promise<string> {
-        const row: Locator = await this.getRowByColumnId(columnId, query);
-        const statusCell = row.locator('.ag-cell[col-id="status"] span[id]');
-        return await statusCell.getAttribute('id');
-    }
-
-    private async getAllRows() {
-        return await this.page.locator('.cxone-grid .ag-center-cols-container .ag-row');
-    }
-
-    private async getRowByColumnId(columnId: string, query: string): Promise<Locator> {
-        const allRows = await this.getAllRows();
-        let index = -1;
-        for (let i = 0; i < allRows.length; i++) {
-            const cell = allRows[i].locator(`.ag-cell[col-id="${columnId}"]`);
-            const textValue = await cell.getText();
-            if (textValue === query) {
-                index = i;
-                break;
-            }
-        }
-        if (index !== -1) {
-            return allRows[index];
-        } else {
-            console.error(`Row not found : columnId - ${columnId} and query - ${query}`);
-        }
-        return index !== -1 ? allRows[index] : undefined;
-    }
-
-    public async openInteraction(columnId: string, query: string) {
-        const row = await this.getRowByColumnId(columnId, query);
-        await row.click();
-    }
-
-    public async getHeaderText() {
-        return await this.page.locator('.header .page-title').gettext();
-    }
 }
